35ca56d7c8dbbcb387e7c1f70c9adad132b8afad
David Viaene <david.viaene@famous.be>
2017-01-12 15:17:03 +0100

    update

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from 'gulp'
 import browserify from 'browserify'
 import source from 'vinyl-source-stream'
 import buffer from 'vinyl-buffer'
+import uglify from 'gulp-uglify'
 
 gulp.task('default', () => {
     const b = browserify({entries: 'src/index.js', debug: false})
@@ -10,5 +11,6 @@ gulp.task('default', () => {
     return b.bundle()
     .pipe(source('index.js')) //will convert text streams from browserify.bundle() to vinyl streams (gulp)
     .pipe(buffer()) //will convert streaming vinyl files to use buffer, some plugins need buffered vinyl objects
+    .pipe(uglify())
     .pipe(gulp.dest('dist/'))
 })
